e8c43e02a804bca4a79deee5dd10cc2b17b45263
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-11-05 16:34:59 +0100

    added General Terms and Conditions checkbox to paymentFormGroup in PaymentComponent

--- CKODemoShop/ClientApp/src/app/components/payment/payment.component.ts
+++ CKODemoShop/ClientApp/src/app/components/payment/payment.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IAddress } from '../../interfaces/address.interface';
 import { ICustomer } from '../../interfaces/customer.interface';
+import { IGtcDisclaimer } from '../../interfaces/gtc-disclaimer.interface';
 
 @Component({
   selector: 'payment-component',
@@ -9,21 +10,28 @@ import { ICustomer } from '../../interfaces/customer.interface';
 })
 export class PaymentComponent {
   isLinear = true;
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
+  customerFormGroup: FormGroup;
+  addressFormGroup: FormGroup;
+  paymentFormGroup: FormGroup;
   selectedPaymentMethod: string;
   paymentMethods: string[] = ['Credit Card', 'iDeal', 'giropay', 'PayPal'];
+  gtcDisclaimer: IGtcDisclaimer = {
+    i_have_read_and_agree: 'I have read and agree with the',
+    g_t_c: 'General Terms & Conditions',
+    g_t_c_uri: 'https://www.checkout.com/legal/terms-and-policies',
+    i_have_verified_and_want_to_pay: 'My Billing Details are correct and I want to continue with the payment'
+  }
+
   customer: ICustomer;
 
   constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
+    this.customerFormGroup = this._formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]]
     });
-    this.secondFormGroup = this._formBuilder.group({
+    this.addressFormGroup = this._formBuilder.group({
       address_line1: ['', Validators.required],
       address_line2: [''],
       city: ['', Validators.required],
@@ -31,20 +39,21 @@ export class PaymentComponent {
       zip: ['', Validators.required],
       country: ['', Validators.required]
     });
-    this.thirdFormGroup = this._formBuilder.group({
-      payment_method: ['', Validators.required]
+    this.paymentFormGroup = this._formBuilder.group({
+      payment_method: ['', Validators.required],
+      gtc: [false, Validators.required]
     })
   }
 
   createCustomer() {
     this.customer = {
       createdOn: Date.now(),
-      name: this.firstFormGroup.get('name').value,
+      name: this.customerFormGroup.get('name').value,
       id: '12345',
-      email: this.firstFormGroup.get('email').value,
-      addresses: [<IAddress>this.secondFormGroup.value],
-      billingAddress: <IAddress>this.secondFormGroup.value,
-      shippingAddress: <IAddress>this.secondFormGroup.value
+      email: this.customerFormGroup.get('email').value,
+      addresses: [<IAddress>this.addressFormGroup.value],
+      billingAddress: <IAddress>this.addressFormGroup.value,
+      shippingAddress: <IAddress>this.addressFormGroup.value
     };
   }
 
