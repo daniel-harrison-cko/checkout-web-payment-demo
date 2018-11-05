99dddd5c759618f477aa77e8a6d1f67ebf493de0
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2018-11-05 15:04:52 +0100

    added toolbar to AppComponent; contains toggle button for view, select for currency, menu for account

--- CKODemoShop/ClientApp/src/app/app.component.ts
+++ CKODemoShop/ClientApp/src/app/app.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ICurrency } from './interfaces/currency.interface';
+import { AppService } from './services/app.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+const CURRENCIES: ICurrency[] = [
+  { iso4217: 'GBP', base: 100 },
+  { iso4217: 'USD', base: 100 },
+  { iso4217: 'EUR', base: 100 }
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription[] = [];
+  title: string = 'Hall of Heroes';
+  currencies: ICurrency[] = CURRENCIES;
+  selectedCurrency: ICurrency;
+  currencyForm: FormGroup;
+
+  constructor(private _appService: AppService, private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.selectedCurrency = this.currencies[0];
+    this.currencyForm = this._formBuilder.group({
+      currency: [this.selectedCurrency, Validators.required]
+    });
+    this.subscriptions.push(
+      this._appService.currency$.subscribe(currency => this.selectedCurrency = currency),
+      //this.currencyForm.valueChanges.subscribe(currency => this._appService.setCurrency(currency)) // TODO: debug selection of currency
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+}
