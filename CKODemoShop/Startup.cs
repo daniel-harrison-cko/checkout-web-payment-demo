6a686b5d70271afcddea8a8f57879f1a32856560
Daniel Münch <daniel.munch@checkout.com>
2019-01-11 17:33:06 +0100

    On ECS the shop must run with a path base. Add support for it.

--- CKODemoShop/Startup.cs
+++ CKODemoShop/Startup.cs
@@ -35,6 +35,11 @@ namespace CKODemoShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if(env.IsProduction())
+            {
+                app.UsePathBase("/demoshop-external");
+            }
+            
             app.UseHealthEndpoint();
             app.UsePingEndpoint();
             
