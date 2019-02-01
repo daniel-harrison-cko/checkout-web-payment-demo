fc5f0003b9723857ba9d74f50db382b614201c7d
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-02-01 08:49:17 +0100

    cleaned OrderService

--- CKODemoShop/ClientApp/src/app/services/order.service.ts
+++ CKODemoShop/ClientApp/src/app/services/order.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { IPayment } from '../interfaces/payment.interface';
-import { IProduct } from '../interfaces/product.interface';
 import { ICurrency } from '../interfaces/currency.interface';
 
 const CURRENCIES: ICurrency[] = [
@@ -19,22 +18,12 @@ const CURRENCIES: ICurrency[] = [
 })
 
 export class OrderService {
-  subscriptions: Subscription[] = [];
-  pointsAmount: number;
-  orderData: IProduct[];
-
-  constructor(private _http: HttpClient) {
-    this.subscriptions.push(
-      this.orderData$.subscribe(product => this.orderData = product)
-    );
-  }
+  constructor(private _http: HttpClient) { }
 
   // Subjects
-  orderDataSource = new BehaviorSubject<IProduct[]>([]);
   private currencySource = new BehaviorSubject<ICurrency>(CURRENCIES[0]);
 
   // Observables
-  orderData$ = this.orderDataSource.asObservable();
   public currency$ = this.currencySource.asObservable();
 
   // Methods
@@ -56,30 +45,5 @@ export class OrderService {
 
   paymentMethodIcon(payment: IPayment): string {
     return payment.source.type == 'card' ? (<string>payment.source["scheme"]).toLowerCase() : payment.source.type;
-  }
-
-  getOrderData(): IProduct[] {
-    return this.orderData;
-  }
-
-  addOrPatchProduct(product: IProduct) {
-    let currentOrderDataSource = this.orderDataSource.value;
-    if (currentOrderDataSource.map(product => product.name).includes(product.name)) {
-      let patchProduct = product;
-      let filteredProduct = currentOrderDataSource.filter(product => product.name == patchProduct.name)[0];
-      let filteredProductIndex = this.orderDataSource.value.indexOf(filteredProduct);
-      this.orderDataSource.value[filteredProductIndex] = patchProduct;
-      this.orderDataSource.next(this.orderDataSource.value);
-    } else {
-      this.orderDataSource.next(currentOrderDataSource.concat([product]));
-    }
-  }
-
-  getTotal(): number {
-    if (this.getOrderData()) {
-      return this.getOrderData().map(t => (t.amount * t.unit)).reduce((acc, value) => acc + value, 0);
-    } else {
-      return 0;
-    }
   }
 }
