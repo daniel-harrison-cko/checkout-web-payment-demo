759af38c0d91c3b2d7b346474badc8050f5329c0
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-04-11 15:40:25 +0200

    added ProductFormComponent to improved payment handling flow

--- CKODemoShop/ClientApp/src/app/components/product-form/product-form.component.ts
+++ CKODemoShop/ClientApp/src/app/components/product-form/product-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, Output, OnInit, EventEmitter, OnDestroy } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { PaymentDetailsService } from 'src/app/services/payment-details.service';
 import { ICurrency } from 'src/app/interfaces/currency.interface';
-import { PaymentsService } from 'src/app/services/payments.service';
 import { distinctUntilChanged } from 'rxjs/operators';
+import { PaymentsService } from 'src/app/services/payments.service';
 
 @Component({
   selector: 'app-product-form',
@@ -12,49 +13,20 @@ import { distinctUntilChanged } from 'rxjs/operators';
 
 export class ProductFormComponent implements OnInit, OnDestroy {
   subscriptions: Subscription[] = [];
-  private _currency: ICurrency;
-  @Output() formReady = new EventEmitter<FormGroup>();
-  productForm: FormGroup;
+  paymentDetails: FormGroup;
+  currency: ICurrency;
 
   constructor(
     private _paymentsService: PaymentsService,
-    private _formBuilder: FormBuilder
+    private _paymentDetailsService: PaymentDetailsService
   ) { }
 
   ngOnInit() {
-    this.productForm = this._formBuilder.group({
-      amount: [null, [Validators.required, Validators.min(0)]],
-      currency: [null, Validators.required]
-    });
-
-    this.formReady.emit(this.productForm);
-
     this.subscriptions.push(
-      this._paymentsService.amount$.subscribe(amount => this.amount = amount),
-      this.getControl('amount').valueChanges.pipe(distinctUntilChanged()).subscribe(amount => this._paymentsService.setAmount(amount)),
-      this._paymentsService.currency$.subscribe(currency => this.currency = currency)
+      this._paymentDetailsService.paymentDetails$.subscribe(paymentDetails => this.paymentDetails = paymentDetails),
+      this.paymentDetails.get('currency').valueChanges.pipe(distinctUntilChanged()).subscribe(currency => this.currency = this._paymentsService.currencies.find(element => element.iso4217 == currency))
     );
-  }
-
-  private getControl(path: string): AbstractControl {
-    return this.productForm.get(path);
-  }
-
-  get amount(): number {
-    return this.getControl('amount').value;
-  }
-
-  set amount(amount: number) {
-    this.getControl('amount').setValue(amount);
-  }
-
-  get currency(): ICurrency {
-    return this._currency;
-  }
-
-  set currency(currency: ICurrency) {
-    this._currency = currency;
-    this.getControl('currency').setValue(currency.iso4217);
+    this.currency = this._paymentsService.currencies.find(element => element.iso4217 == this.paymentDetails.value.currency);
   }
 
   ngOnDestroy() {
