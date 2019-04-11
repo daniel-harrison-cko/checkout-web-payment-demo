6d4d4c5969ee7e821eb9eb2ab01418bba8c99a24
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-04-11 08:43:04 +0200

    added PaymentDetailsService

---
+++ CKODemoShop/ClientApp/src/app/services/payment-details.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PaymentDetailsService {
+  constructor(private _formBuilder: FormBuilder) { }
+
+  // Subjects
+  private paymentDetailsSource = new BehaviorSubject<FormGroup>(this.paymentDetailsTemplate);
+
+  // Observables
+  public paymentDetails$ = this.paymentDetailsSource.asObservable();
+
+  // Methods
+  public updatePaymentDetails(paymentDetails: FormGroup) {
+    this.paymentDetailsSource.next(paymentDetails);
+  }
+
+  get paymentDetailsTemplate(): FormGroup {
+    let paymentDetailsTemplate = this._formBuilder.group({
+      source: this._formBuilder.group({
+        type: [null, Validators.required]
+      }),
+      customer: this._formBuilder.group({
+        id: null,
+        email: ['bruce@wayne-enterprises.com', Validators.required],
+        name: ['Bruce Wayne', Validators.required]
+      }),
+      billing_address: this._formBuilder.group({
+        address_line1: ['Bruce Wayne'],
+        address_line2: ['Wayne Plaza 1', Validators.required],
+        city: ['Gotham City', Validators.required],
+        state: ['NJ'],
+        zip: ['12345', Validators.required],
+        country: ['US', Validators.required]
+      }),
+      shipping: this._formBuilder.group({
+        address: this._formBuilder.group({
+          address_line1: ['Bruce Wayne', Validators.required],
+          address_line2: ['Wayne Plaza 1', Validators.required],
+          city: ['Gotham City', Validators.required],
+          state: ['NJ'],
+          zip: ['12345', Validators.required],
+          country: ['US', Validators.required]
+        }),
+        phone: this._formBuilder.group({
+          country_code: null,
+          number: null
+        })
+      })
+    });
+    return paymentDetailsTemplate;
+  }
+}
