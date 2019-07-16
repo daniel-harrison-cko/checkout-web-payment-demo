69e91113d1857e1d2eb5fb02f1abcc6959af647a
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-07-16 17:09:47 +0200

    fixed base path for corrrect MVC routing

--- CKODemoShop/Startup.cs
+++ CKODemoShop/Startup.cs
@@ -56,11 +56,7 @@ namespace CKODemoShop
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (!env.IsDevelopment())
-            {
-                app.UsePathBase("/demoshop-external");
-            }
-            
+            app.UsePathBase("/demoshop-external");
             app.UseHealthEndpoint();
             app.UsePingEndpoint();
 
