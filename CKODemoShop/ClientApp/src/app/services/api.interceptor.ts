a6c2fcd9e6babfb6a16bb0a76a46062f5563a08b
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-07-18 11:48:51 +0200

    added dynamicity to interceptor via APP_BASE_HREF injection

--- CKODemoShop/ClientApp/src/app/services/api.interceptor.ts
+++ CKODemoShop/ClientApp/src/app/services/api.interceptor.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common'; 
 
 @Injectable()
 export class APIInterceptor implements HttpInterceptor {
+  constructor(@Inject(APP_BASE_HREF) private appBaseHref: string) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     if (req.url.startsWith('http')) {
       return next.handle(req);
     } else {
-      const apiReq = req.clone({ url: `/demoshop-external${req.url}` });
+      console.log(this.appBaseHref);
+      const apiReq = req.clone({ url: `${this.appBaseHref}${req.url}` });
       return next.handle(apiReq);
     }
   }
