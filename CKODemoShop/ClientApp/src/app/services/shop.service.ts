8e52af7232bc41fafdd37687fee5aaf9c52949cd
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-08-13 15:53:20 +0200

    added endpoint to get public key from server environment; updated frontend public key uses to request the key from backend instead of having it hardcoded

--- CKODemoShop/ClientApp/src/app/services/shop.service.ts
+++ CKODemoShop/ClientApp/src/app/services/shop.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
-  constructor(private _http: HttpClient) { }
+  public publicKey: string;
+
+  constructor(private _http: HttpClient) {
+    this.getPublicKey().pipe(take(1)).subscribe(response => this.publicKey = response.body.public_key);
+  }
 
   getReference(): Observable<HttpResponse<any>> {
     return this._http.get<string>(`/api/shop/references`, {observe: 'response'});
+  }
+
+  private getPublicKey(): Observable<HttpResponse<any>> {
+    return this._http.get<string>(`/api/config/publickey`, { observe: 'response' });
   }
 }
--- CKODemoShop/Controllers/ConfigController.cs
+++ CKODemoShop/Controllers/ConfigController.cs
@@ -29,5 +29,21 @@ namespace CKODemoShop.Controllers
             };
             return Ok(model);
         }
+
+        [HttpGet("[action]", Name = "GetPublicKey")]
+        [ActionName("PublicKey")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(404)]
+        public IActionResult GetPublicKey()
+        {
+            try
+            {
+                return Ok(new { PublicKey = Environment.GetEnvironmentVariable("CKO_PUBLIC_KEY") });
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
     }
 }
