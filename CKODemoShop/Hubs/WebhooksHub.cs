dbfeec4269ecc2061f0323ddfeac87f8d20241dc
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-08-19 18:07:38 +0200

    added some comments

--- CKODemoShop/Hubs/WebhooksHub.cs
+++ CKODemoShop/Hubs/WebhooksHub.cs
@@ -6,11 +6,14 @@ namespace CKODemoShop.Hubs
 {
     public class WebhooksHub : Hub<IWebhooksHubClient> {
 
+        // overrides base method of new incoming connection to Websocket HUB
         public override async Task OnConnectedAsync()
         {
             var publicKey = Environment.GetEnvironmentVariable("CKO_PUBLIC_KEY");
             Console.WriteLine($"Mapped Websocket Connection {Context.ConnectionId} to {publicKey}");
+            // adds the incoming connection id to a common group
             await Groups.AddToGroupAsync(Context.ConnectionId, publicKey);
+            // gives feedback to the client about mapping
             await Clients.Caller.ConnectionIdMappedToGroup(Context.ConnectionId, publicKey);
             await base.OnConnectedAsync();
         }
