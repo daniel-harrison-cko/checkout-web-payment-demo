60c6463422fb059f41e9ed7d898eb2935ae99d21
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-08-23 13:16:08 +0200

    fixed setters and default values for SecretKey and PublicKey in CheckoutAouOptions

--- CKODemoShop/Configuration/CheckoutApiOptions.cs
+++ CKODemoShop/Configuration/CheckoutApiOptions.cs
@@ -1,9 +1,32 @@
+using System;
+
 namespace CKODemoShop.Configuration
 {
     public class CheckoutApiOptions 
     {
-        public string SecretKey { get; set; }
-        public string PublicKey { get; set; }
+        private string _SecretKey = Environment.GetEnvironmentVariable("CKO_SECRET_KEY");
+        private string _PublicKey = Environment.GetEnvironmentVariable("CKO_PUBLIC_KEY");
+
+        public string SecretKey {
+            get
+            {
+                return _SecretKey;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) _SecretKey = value;
+            }
+        }
+        public string PublicKey {
+            get
+            {
+                return _PublicKey;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value)) _PublicKey = value;
+            }
+        }
         public string GatewayUri { get; set; }
     }
 }
