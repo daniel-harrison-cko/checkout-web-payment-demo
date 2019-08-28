f691be5bd69681d1217073f1ca50e5e00db1a6d2
philippe-leonhardt-cko <35501037+philippe-leonhardt-cko@users.noreply.github.com>
2019-08-28 11:41:09 +0200

    removed redundant ConfiguredFromEnvironment() method from CheckoutApiFactory

--- CKODemoShop/Checkout/CheckoutApiFactory.cs
+++ CKODemoShop/Checkout/CheckoutApiFactory.cs
@@ -6,16 +6,6 @@ namespace CKODemoShop.Checkout
 {
     public static class CheckoutApiFactory
     {
-        public static CheckoutApi ConfiguredFromEnvironment()
-        {
-            var api = CheckoutApi.Create(
-               secretKey: Environment.GetEnvironmentVariable("CHECKOUTAPIOPTIONS__SECRETKEY"),
-               publicKey: Environment.GetEnvironmentVariable("CHECKOUTAPIOPTIONS__PUBLICKEY"),
-               uri: Environment.GetEnvironmentVariable("CHECKOUTAPIOPTIONS__GATEWAYURI")
-            );
-            return api;
-        }
-
         public static CheckoutApi ConfiguredFromOptions(CheckoutApiOptions options)
         {
             return CheckoutApi.Create(
